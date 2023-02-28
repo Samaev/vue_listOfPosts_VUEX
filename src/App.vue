71f7bbffdc0d0f0e317c604b2c27ff38abef8c10
Andrey Samaev <andreysamaev@gmail.com>
2023-02-28 11:39:59 +0200

    updated components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      List of Posts
+    </h1>
+    <PostList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from './components/PostList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostList
   }
 }
 </script>
---
+++ src/components/PostList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    Post
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  props: {
+  },
+  methods: {
+    getPosts: function () {
+      fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+        this.$store.state.posts = response.data;
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
