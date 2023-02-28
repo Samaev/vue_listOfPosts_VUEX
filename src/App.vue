a878c063300ee08be0095927b359485be877ccf3
Andrey Samaev <andreysamaev@gmail.com>
2023-02-28 13:34:20 +0200

    dropdown filter is added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <h1>
-      List of Posts
+    <h1 class="row">
+      <h3 class="col">List of Posts</h3>
+      <select class="form-control col" v-model="selectedUser">
+        <option>All Users</option>
+        <option v-for="user in allUsers" v-bind:value="user" v-bind:key="user.id" >
+          {{ user.name }}
+        </option>
+      </select>
     </h1>
-    <div class="card mt-2" v-for="post in allPosts" v-bind:key="post.id">
+    <div class="card mt-2" v-for="post in allPosts.filter(post=>post.userId === selectedUser.id)" v-bind:key="post.id">
       <div class="card-header">{{ allUsers.find(user=>user.id === post.userId).name }}</div>
       <div class="card-title">
         {{ post.title }}
@@ -20,6 +26,11 @@ import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      selectedUser: {}
+    }
+  },
   computed: mapGetters(['allPosts', 'allUsers']),
   methods: mapActions(['fetchPosts', 'fetchUsers']),
   async mounted () {
